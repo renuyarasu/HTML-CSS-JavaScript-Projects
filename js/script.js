502232967c2bf0f4b1b82116af23340a32357a06
Renuka Prasad <renu.yarasu@gmail.com>
2023-02-27 19:44:49 +0530

    Project 1 -  Background Image Scroll Effect - Js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // HTML, CSS & JavaScript Projects
+const bg_image = document.getElementById('bg-image');
 
+window.addEventListener('scroll', () => {
+    updateImage()
+})
+
+function updateImage() {
+    bg_image.style.opacity = 1 - window.pageYOffset / 800;
+    bg_image.style.backgroundSize = cover - window.pageYOffset + '%';
+}
