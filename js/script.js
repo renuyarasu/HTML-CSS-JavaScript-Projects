ac6b00f216ee5b794a144dc8f27ac90f04f37bc6
Renuka Prasad <renu.yarasu@gmail.com>
2023-03-01 23:38:19 +0530

    Project 3 - Loading Bar - JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // HTML, CSS & JavaScript Projects
+
+const counter = document.querySelector('.counter');
+
+const bar_front = document.querySelector('.loading-bar-front');
+
+let idx = 0;
+
+
+function updateNumber() {
+    counter.innerHTML = idx + '%';
+    bar_front.style.width = idx + '%';
+    idx++;
+    
+    if (idx < 101) {
+        setTimeout(updateNumber, 100)
+    }
+    
+}
+updateNumber();
