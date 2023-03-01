55eff8aba5aadcc8162d7ef9eba7384fd7aef5a2
Renuka Prasad <renu.yarasu@gmail.com>
2023-03-01 23:13:20 +0530

    Project 2 - Mouse Event - JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // HTML, CSS & JavaScript Projects
+
+const container = document.querySelector('.container');
+
+window.addEventListener('mouseover', (e) => {
+    container.innerHTML = `
+   
+        <div class="mouse-event">            
+            <h1>${e.clientX}</h1>
+            <h4>Mouse X Position (px)</h4>
+        </div>
+         <div class="mouse-event">
+            <h1>${e.clientY}</h1>
+            <h4>Mouse Y Position (px)</h4>
+        </div>
+
+
+    `
+})
